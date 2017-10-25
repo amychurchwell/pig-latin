4559375f6f3bae88e52ab6237617221f908b3b46
Amy Churchwell and Lena Ha <amy@>
2017-10-25 12:54:58 -0700

    add subtract first character to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
+var vowels = ["a", "e", "i", "o", "u"]
+var y = ["y"]
 
-var pigLatin = function(sentence) {
-  for
-}
+
+
+var pigLatin = function(sentence){
+   for (var v = 0; v < vowels.length; v++){
+    if (vowels[v] === sentence[0])
+      sentence.charAt(0);
+   }
+   $("#output").text(sentence + "way");
+ }
 
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
+    $("#output").text(sentence + "way");
+    event.preventDefault();
 
-    var sentence = $("input#input").val();
+    var input = $("#textInput").val();
+    var sentence = input.substr(1);
+
+//    var temporary = input.split();
+//    var temp2 = temporary.shift();
+//    var sentence = temp2.join();
+
+    pigLatin(sentence);
 
   });
 });
