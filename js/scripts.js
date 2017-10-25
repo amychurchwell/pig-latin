31d1753ab9037c76732cef9c06b7bda579eaeadb
Amy Churchwell and Lena Ha <lena@>
2017-10-25 13:08:31 -0700

    add pig latin for consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 var vowels = ["a", "e", "i", "o", "u"]
-var y = ["y"]
 
+//sentence.charAt(0);
 
 
 var pigLatin = function(sentence){
-   for (var v = 0; v < vowels.length; v++){
-    if (vowels[v] === sentence[0])
-      sentence.charAt(0);
-   }
-   $("#output").text(sentence + "way");
- }
+  for (var v = 0; v < vowels.length; v++){
+    if (vowels[v] === sentence[0]){
+      $("#output").text(sentence + "way");
+    }else{
+      var letter1 = sentence.charAt(0);
+      var conSentence = sentence.substr(1);
+      $("#output").text(conSentence + letter1 + "ay")
+    }
+  }
+}
 
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     $("#output").text(sentence + "way");
     event.preventDefault();
 
-    var input = $("#textInput").val();
-    var sentence = input.substr(1);
+    var sentence = $("#textInput").val();
+
 
 //    var temporary = input.split();
 //    var temp2 = temporary.shift();
